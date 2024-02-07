7a7e793e318e6941a914cf9ceb0dcc7e865cc42f
sauron432 <132927445+sauron432@users.noreply.github.com>
2024-02-07 15:34:09 +0545

    now working

--- .history/app/Providers/AppServiceProvider_20240207144435.php
+++ .history/app/Providers/AppServiceProvider_20240207144435.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        \Schema::defaultStringLength(191);
+        \\
+        Schema::defaultStringLength(191);
     }
 }
